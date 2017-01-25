0f8f6b25f9524576b78d530c4160bc228f59ec37
Antoine BONTE <bontea@meleze25.iut-infobio.priv.univ-lille1.fr>
2017-01-25 11:11:26 +0100

    dossier Fb_like avancé

---
+++ WEB-INF/classes/db/Connexion.java
@@ -0,0 +1,67 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.*;
+
+public class Connexion {
+    private String DBPath = "";
+    private Connection connection = null;
+    private Statement statement = null;
+ 
+    public Connexion(String dBPath) {
+        DBPath = dBPath;
+    }
+
+    public Connection getConnection(){
+	return this.connection;
+    }
+ 
+    public void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DBPath);
+            statement = connection.createStatement();
+            System.out.println("Connexion a " + DBPath + " avec succès");
+        } catch (ClassNotFoundException notFoundException) {
+            notFoundException.printStackTrace();
+            System.out.println("Erreur de connexion");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            System.out.println("Erreur de connexion");
+        }
+    }
+ 
+    public void close() {
+        try {
+            connection.close();
+            //statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet select(String rq) {
+       ResultSet resultat= null;
+       try {
+           resultat = statement.executeQuery(rq);	 
+       } catch (SQLException e) {
+           e.printStackTrace();
+           System.out.println("Erreur dans la requête : " + rq);
+       }
+       return resultat;  
+   }
+
+    public void IUD(String rq){	
+	try{
+	    statement.executeUpdate(rq);
+	}catch(SQLException e){
+	   e.printStackTrace();
+           System.out.println("Erreur dans la requête : " + rq);
+	}
+       
+    }
+    
+}
---
+++ script.sql
@@ -0,0 +1,71 @@
+-- Creator:       MySQL Workbench 6.3.8/ExportSQLite Plugin 0.1.0
+-- Author:        Clément Cantraine
+-- Caption:       New Model
+-- Project:       Name of the project
+-- Changed:       2017-01-04 09:14
+-- Created:       2017-01-04 08:25
+PRAGMA foreign_keys = OFF;
+
+BEGIN;
+
+DROP TABLE IF EXIST visibilitee;
+DROP TABLE IF EXIST utilisateurs;
+DROP TABLE IF EXIST amitiees;
+DROP TABLE IF EXIST publications;
+DROP TABLE IF EXIST jaime;
+
+
+CREATE TABLE visibilitee(
+  "idVisibilitee" INTEGER PRIMARY KEY NOT NULL,
+  "libelle" VARCHAR(45)
+);
+
+CREATE TABLE utilisateurs(
+  "idUtilisateur" INTEGER PRIMARY KEY NOT NULL,
+  "nom" VARCHAR(80),
+  "prenom" VARCHAR(80),
+  "datenaissance" DATE,
+  "email" VARCHAR(80),
+  "mdp" VARCHAR(40),
+  CONSTRAINT "idVisi"
+    FOREIGN KEY("idUtilisateur")
+    REFERENCES "visibilitee"("idVisibilitee")
+);
+
+CREATE TABLE amitiees(
+  "idamitiees" INTEGER PRIMARY KEY NOT NULL,
+  "dateamitiee" DATE,
+  CONSTRAINT "userA"
+    FOREIGN KEY("idamitiees")
+    REFERENCES "utilisateurs"("idUtilisateur"),
+  CONSTRAINT "userB"
+    FOREIGN KEY("idamitiees")
+    REFERENCES "utilisateurs"("idUtilisateur")
+);
+CREATE TABLE publications(
+  "idPublication" INTEGER PRIMARY KEY NOT NULL,
+  "contenu" LONGTEXT,
+  "date" DATETIME,
+  CONSTRAINT "publicationscol_UNIQUE"
+    UNIQUE("contenu"),
+  CONSTRAINT "auteur"
+    FOREIGN KEY("idPublication")
+    REFERENCES "utilisateurs"("idUtilisateur")
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE jaime(
+  "idJaime" INTEGER PRIMARY KEY NOT NULL,
+  CONSTRAINT "idPublication"
+    FOREIGN KEY("idJaime")
+    REFERENCES "publications"("idPublication")
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT "idUtilisateur"
+    FOREIGN KEY("idJaime")
+    REFERENCES "utilisateurs"("idUtilisateur")
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+COMMIT;
